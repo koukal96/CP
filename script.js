c066c8a62656002d827fe3311f5ec55a66b1af26
koukal96 <r1mail@seznam.cz>
2026-03-30 20:07:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// Mapa
-const coords = [49.450889, 17.311747];
-const map = L.map('map', {scrollWheelZoom: false, zoomControl: false}).setView(coords, 14);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(map);
-const icon = L.divIcon({className: 'status-dot'});
-L.marker(coords, {icon}).addTo(map);
-
-// Interaktivní pohyb karet (tilt efekt simulace)
-document.querySelectorAll('.tech-card').forEach(card => {
-    card.addEventListener('mousemove', e => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        card.style.setProperty('--x', `${x}px`);
-        card.style.setProperty('--y', `${y}px`);
-    });
-});
-
-// Automatický Glitch efekt pro H1
-const mainTitle = document.querySelector('.glitch-text');
-setInterval(() => {
-    mainTitle.style.opacity = Math.random() > 0.95 ? '0.5' : '1';
-}, 100);
 // 1. Logika pro změnu vzhledu navigace při scrollování
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
